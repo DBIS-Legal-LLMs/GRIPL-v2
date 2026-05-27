cdacaec228c4eda15e9f661e4698db8847f0a1d3
Lukas <51384394+lukasbraundev@users.noreply.github.com>
2026-05-27 12:35:40 +0200

    added favicon controller

---
+++ gripl/gripl-backend/src/main/kotlin/de/mertendieckmann/griplbackend/adapter/web/FaviconController.kt
@@ -0,0 +1,13 @@
+﻿package de.mertendieckmann.griplbackend.adapter.web
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class FaviconController {
+    @GetMapping("/favicon.ico")
+    fun favicon(): ResponseEntity<Void> {
+        return ResponseEntity.noContent().build()
+    }
+}
